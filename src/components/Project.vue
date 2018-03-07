66b70175b3bcd943acbf6589b780c4cedcc31b80
omelendrez <omar.melendrez@am.sony.com>
2018-03-07 15:54:00 -0300

    Add projects

---
+++ src/components/Project.vue
@@ -0,0 +1,130 @@
+<template>
+
+  <b-container class="project">
+    <h3 class="text-center">Project</h3>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show" id="addForm">
+
+      <b-form-group horizontal label="Organization" label-for="organization_id">
+        <b-form-select v-model="form.organization_id" :options="organizations" class="mb-3" required/>
+      </b-form-group>
+
+      <b-form-group horizontal label="Code" label-for="code">
+        <b-form-input id="code" v-model.trim="form.code" required></b-form-input>
+      </b-form-group>
+
+      <b-form-group horizontal label="Name" label-for="name">
+        <b-form-input id="name" v-model.trim="form.name" required></b-form-input>
+      </b-form-group>
+
+      <Buttons/>
+
+    </b-form>
+  </b-container>
+
+</template>
+
+<script>
+import Store from "../store/store";
+import Buttons from "./lib/Buttons";
+
+export default {
+  name: "Project",
+  data() {
+    return {
+      show: true,
+      form: {
+        id: 0,
+        code: "",
+        name: "",
+        organization_id: 0
+      }
+    };
+  },
+  components: {
+    Buttons
+  },
+  watch: {
+    results() {
+      const results = Store.state.results;
+      if (results.error) {
+        return;
+      }
+      this.$router.push({ name: "Projects" });
+    }
+  },
+  computed: {
+    organizations() {
+      const organizations = Store.state.activeOrganizations;
+      if (!organizations) {
+        return;
+      }
+      const options = [];
+      for (let i = 0; i < organizations.length; i++) {
+        if (
+          organizations[i].id === Store.state.user.organization_id ||
+          Store.state.globalAdmin
+        ) {
+          options.push({
+            value: organizations[i].id,
+            text: organizations[i].name
+          });
+        }
+      }
+      return options;
+    },
+    results() {
+      return Store.state.results;
+    },
+    isLogged() {
+      return Store.state.user.id;
+    },
+    item() {
+      return Store.state.record;
+    },
+    organization_id() {
+      return Store.state.user.organization_id;
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      Store.dispatch("SAVE_PROJECT", this.form);
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => {
+        this.$router.push({ name: "Projects" });
+      });
+    }
+  },
+  created() {
+    if (!this.isLogged) {
+      this.$router.push({ name: "Login" });
+      return;
+    }
+    Store.dispatch("LOAD_ORGANIZATIONS");
+    if (this.item) {
+      this.form.id = this.item.id;
+      this.form.code = this.item.code;
+      this.form.name = this.item.name;
+      this.form.organization_id = this.item.organization_id;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.project {
+  background-color: white;
+  padding: 60px;
+  margin-top: 18px;
+}
+#addForm {
+  margin: 0 auto;
+  max-width: 800px;
+  padding-top: 40px;
+}
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,130 @@
+<template>
+  <b-container class="projects" fluid>
+
+    <Add />
+
+    <b-form-group class="filter-form">
+      <b-input-group>
+        <b-form-input v-model="filter" placeholder="Type here for a quick search" />
+        <b-btn :disabled="!filter" @click="filter = ''" variant="info" class="reset-button">Reset</b-btn>
+      </b-input-group>
+    </b-form-group>
+
+    <b-table small hover outlined :items="projects.rows" :fields="fields" :filter="filter" :per-page="perPage" :current-page="currentPage" head-variant="light">
+      <template slot="actions" slot-scope="cell">
+        <b-btn variant="info" @click.stop="editItem(cell.item)">Modify</b-btn>
+        <b-btn v-if="cell.item.status_id === 1" variant="danger" @click.stop="deleteItem(cell.item, 1)">Inactivate</b-btn>
+        <b-btn v-else variant="success" @click.stop="deleteItem(cell.item, 0)">Re-activate</b-btn>
+      </template>
+      <template slot="table-caption">
+        {{projects.count}} registros
+      </template>
+    </b-table>
+
+    <b-pagination :total-rows="projects.count" :per-page="perPage" v-model="currentPage" variant="info" />
+
+    <b-modal id="modal-center" title="Inactivate" v-model="deleteShow" @ok="handleOk" ok-title="Yes. Inactivate" cancel-title="No. Leave it Active" ok-variant="danger" cancel-variant="success">
+      <p class="my-4">Are you sure you want to inactivate
+        <strong>{{ selectedItem.code }} - {{ selectedItem.name }} </strong>?</p>
+    </b-modal>
+  </b-container>
+</template>
+
+<script>
+import Store from "../store/store";
+import Add from "./lib/Add";
+const fields = require("./lib/Fields").projects;
+const commonFields = require("./lib/Fields").commonFields;
+const actions = require("./lib/Fields").actions;
+const org = require("./lib/Fields").org;
+
+export default {
+  name: "Projects",
+  data() {
+    return {
+      perPage: 10,
+      currentPage: 1,
+      filter: null,
+      deleteShow: false,
+      selectedItem: {
+        code: "",
+        name: ""
+      },
+      fields: fields
+    };
+  },
+  methods: {
+    editItem(item) {
+      Store.dispatch("ADD_ITEM", item);
+      this.$router.push({ name: "Project" });
+    },
+    deleteItem(item, type) {
+      this.selectedItem = item;
+      if (type === 1) {
+        this.deleteShow = true;
+      } else {
+        this.handleOk();
+      }
+    },
+    handleOk() {
+      Store.dispatch("DELETE_PROJECT", this.selectedItem);
+    }
+  },
+  watch: {
+    results() {
+      const results = Store.state.results;
+      if (results.error) {
+        return;
+      }
+      Store.dispatch("LOAD_PROJECTS");
+    }
+  },
+  computed: {
+    results() {
+      return Store.state.results;
+    },
+    isLogged() {
+      return Store.state.user.id;
+    },
+    projects() {
+      return Store.state.projects;
+    }
+  },
+  created() {
+    if (!this.isLogged) {
+      this.$router.push({ name: "Login" });
+      return;
+    }
+    Store.dispatch("SET_MENU_OPTION", this.$route.path);
+    Store.dispatch("LOAD_PROJECTS");
+
+    this.fields.unshift(org);
+    this.fields.push(...commonFields);
+    if (Store.state.admin) {
+      this.fields.push(...actions);
+    }
+  },
+  components: {
+    Add
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.projects {
+  background-color: white;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+.add-button {
+  float: right;
+}
+.filter-form {
+  max-width: 30%;
+  margin-bottom: 20px;
+}
+.reset-button {
+  margin-left: 10px;
+}
+</style>
