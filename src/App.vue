bdf72164eed8a7f91ecece3839383bd418f8f8b8
omelendrez <omar.melendrez@gmail.com>
2018-04-02 17:56:34 -0300

    Font defaults

--- src/App.vue
+++ src/App.vue
@@ -18,17 +18,22 @@ export default {
 
 <style>
 html {
-  font-size: 90%;
+  font-size: small;
 }
 body {
   background-color: rgb(240, 240, 240) !important;
-  font-family: 'Roboto', sans-serif;
+  font-family: Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+    'Segoe UI Symbol' !important;
 }
 .table-inactive {
   color: red;
 }
 .table-selected {
   background-color: #f5f3ee;
+}
+table {
+  font-size: smaller;
 }
 table.b-table.b-table-stacked > tbody > tr > [data-label] {
   grid-template-columns: 10% auto !important;
